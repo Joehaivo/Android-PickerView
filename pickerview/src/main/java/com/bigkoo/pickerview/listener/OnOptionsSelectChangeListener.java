6a20b7ff6a0e12863d0a438f6004aac72c47f357
youdehai <youdehai@wasu.com>
2020-10-13 18:35:46 +0800

    [add]

--- pickerview/src/main/java/com/bigkoo/pickerview/listener/OnOptionsSelectChangeListener.java
+++ pickerview/src/main/java/com/bigkoo/pickerview/listener/OnOptionsSelectChangeListener.java
@@ -6,7 +6,7 @@ import com.bigkoo.pickerview.view.OptionsPickerView;
  * Created by xiaosong on 2018/3/20.
  */
 
-public interface OnOptionsSelectChangeListener {
+public interface OnOptionsSelectChangeListener<T> {
 
-    <T> void onOptionsSelectChanged(int options1, int options2, int options3, OptionsPickerView<T> pickerView);
+    void onOptionsSelectChanged(int options1, int options2, int options3, OptionsPickerView<T> pickerView);
 }
